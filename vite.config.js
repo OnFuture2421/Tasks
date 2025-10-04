bbf1cb20b4c45870ae991334bedc2849d8f4f54b
Eyad <iyad.a.samy@outlook.com>
2025-10-04 22:10:01 +0100

    Update project name and homepage in package.json; change title in index.html; adjust base path in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/CSSTUTORIAL/' : '/', // ✅ smart!
   plugins: [react()],
-  server: {
-    port: 2424,
-  }
+  base: '/Tasks/'  // <-- matches the repo name
 })
